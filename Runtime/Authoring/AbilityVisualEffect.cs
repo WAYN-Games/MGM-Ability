9262c280d1cadf533350d2891f73514b6d6dafc6
WAYNGROUP <WAYNGROUP@users.noreply.github.com>
2020-08-01 11:32:26 +0200

    Adding basic vfx. Working webgl build. Working non interactible UI.

---
+++ Runtime/Authoring/AbilityVisualEffect.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class AbilityVisualEffect : MonoBehaviour
+{
+    private VisualEffect _vfx;
+    private bool IsPlaying;
+
+    void Awake()
+    {
+        _vfx = GetComponent<VisualEffect>();
+    }
+
+    private void Start()
+    {
+        Play();
+    }
+
+    public void Play()
+    {
+        _vfx.Play();
+        IsPlaying = true;
+    }
+
+    public void Stop()
+    {
+        _vfx.Stop();
+        IsPlaying = false;
+    }
+
+    void Update()
+    {
+        if (!IsPlaying && _vfx.aliveParticleCount <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
