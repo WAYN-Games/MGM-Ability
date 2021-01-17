31bca2698cf094a416b75797b40938d0299d4a49
WAYNGROUP <WAYNGROUP@users.noreply.github.com>
2021-01-17 10:39:04 +0100

    add suport for no subscene workflow.

--- Runtime/Scripts/Authoring/AbilityAuthoring.cs
+++ Runtime/Scripts/Authoring/AbilityAuthoring.cs
@@ -7,11 +7,11 @@ using UnityEngine.AddressableAssets;
 
 using WaynGroup.Mgm.Ability;
 
-#if UNITY_EDITOR
+
 [DisallowMultipleComponent]
 public class AbilityAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
-    [Tooltip("Intial time needed for all abilities to be ready. This allow the system to initialise before an ability can be activated.")]
+    [Tooltip("Intial time used for all abilities to be ready in case of runtime conversion. (The subscene workflow is prefered).")]
     public float InitialGlobalCoolDown = 2.5f;
 
     [Tooltip("List of Scriptable Ability addressable asset reference.")]
@@ -20,14 +20,20 @@ public class AbilityAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         DynamicBuffer<AbilityBufferElement> abilityBuffer = dstManager.AddBuffer<AbilityBufferElement>(entity);
+
         for (int i = 0; i < Abilities.Count; i++)
         {
+#if UNITY_EDITOR
             conversionSystem.DeclareAssetDependency(gameObject, Abilities[i].editorAsset);
             AbilityHelper.AddAbility(Abilities[i].editorAsset, ref abilityBuffer);
+#endif
+#if !UNITY_EDITOR
+            Debug.LogWarning($"Runtime conversion through AbilityAuthoring does not support asset loading. It will take default value instead");
+            AbilityHelper.AddAbility(AbilityHelper.ComputeAbilityIdFromGuid(Abilities[i].AssetGUID), InitialGlobalCoolDown, ref abilityBuffer);
+#endif
         }
     }
 
 
 
 }
-#endif
